83342d34c51f5e125a7d94e6227838f16092246e
Student Laptop_7/19_1 <studentlaptop_719_1@Students-MacBook-Pro.local>
2019-10-01 23:17:28 -0700

    Added test

--- app.py
+++ app.py
@@ -28,28 +28,34 @@ def playlists_index():
 @app.route('/playlists/new')
 def playlists_new():
     """Create a new playlist."""
-    return render_template('playlists_new.html')
+    return render_template('playlists_new.html', playlist={}, title='New Playlist')
 
-@app.route('/playlists/<playlist_id>')
-def playlists_show(playlist_id):
-    """Show a single playlist."""
+@app.route('/playlists/<playlist_id>/edit')
+def playlists_edit(playlist_id):
+    """Show the Edit form for a playlist."""
     playlist = playlists.find_one({'_id': ObjectId(playlist_id)})
-    return render_template('playlists_show.html', playlist=playlist)
+    return render_template('playlists_edit.html', playlist=playlist, title='Edit Playlist')
     # return f'My ID is {playlist_id}'
 
 @app.route('/playlists', methods=['POST'])
-def playlists_submit():
-    """Submit a new playlist."""
-    # print(request.form.to_dict())
-    playlist = {
+def playlists_update(playlist_id):
+    """Submit an edited playlist."""
+    updated_playlist = {
         'title': request.form.get('title'),
         'description': request.form.get('description'),
         'videos': request.form.get('videos').split()
     }
-    playlist_id = playlists.insert_one(playlist).inserted_id
+    playlists.update_one(
+        {'_id': ObjectId(playlist_id)},
+        {'$set': updated_playlist})
     return redirect(url_for('playlists_show', playlist_id=playlist_id))
-    # playlists.insert_one(playlist)
-    # return redirect(url_for('playlists_index'))
+
+@app.route('/playlists/<playlist_id>/delete', methods=['POST'])
+def playlists_delete(playlist_id):
+    """Delete One playlist."""
+    playlists.delete_one({'_id': ObjectId(playlist_id)})
+    return redirect(url_for('playlists_index'))
+
 
 if __name__ == '__main__':
     app.run(debug=True)
